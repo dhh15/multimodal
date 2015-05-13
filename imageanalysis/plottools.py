f68a3b6900b75aa84ae5f82e45eb4be69efe2c69
Aaro Salosensaari <aaro.salosensaari@cs.helsinki.fi>
2015-05-13 14:35:37 +0300

    useful plot cmd (while working in ipython)

---
+++ imageanalysis/plottools.py
@@ -0,0 +1,19 @@
+# encoding: utf-8
+
+from matplotlib import pyplot as plt
+
+
+# plottikomento talteen
+
+def plot_3histo(image):
+    """Plot a three channel histo of an image with matplotlib
+
+    :histo: TODO
+    :returns: TODO
+
+    """
+    plt.figure()
+    for i in range(0,3):
+        histr = cv2.calcHist([tc],[i],None,[8],[0,256])
+        plt.plot(histr)
+
