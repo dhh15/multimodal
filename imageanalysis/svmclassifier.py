8ba830d14f82f36f4aee13fb8530176dc3fdf7ea
Aaro Salosensaari <aaro.salosensaari@cs.helsinki.fi>
2015-05-13 16:49:02 +0300

    create actual train data from preclassified clips
    'nuff said

--- imageanalysis/svmclassifier.py
+++ imageanalysis/svmclassifier.py
@@ -5,3 +5,30 @@
 import numpy as np
 import cv2
 
+# helper function:
+
+def calc_hist(clip):
+    return cv2.calcHist([clip], [0,1,2], None, [8,8,8], [0, 256, 0,256, 0,256])
+
+
+def get_flathists(dirname):
+    dirlist = os.listdir(dirname)
+    flathists = []
+    for clip in dirlist:
+        clip = cv2.imread(dirname + tclip)
+        hist3c = calc_hist(clip)
+        hist_flat = hist3c.flatten()
+        flathists.append(hist_flat)
+
+    return flathists
+
+# get training data
+
+text_dir = "folder/"
+image_dir = "another_folder/"
+
+text_hists = np.array(get_flathists(text_dir))
+image_hists = np.array(get_flathists(image_dir))
+
+
+
