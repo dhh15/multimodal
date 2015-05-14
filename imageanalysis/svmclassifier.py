3434956ff0cc7e8ccbf6f2c3f21ef98c06fb9a84
Aaro Salosensaari <aaro.salosensaari@cs.helsinki.fi>
2015-05-14 12:16:52 +0300

    test different C, gamma

--- imageanalysis/svmclassifier.py
+++ imageanalysis/svmclassifier.py
@@ -49,25 +49,7 @@ text_responses = np.float32(np.repeat(TEXT_C,len(text_hists))[:,np.newaxis])
 image_responses = np.float32(np.repeat(IMAGE_C,len(image_hists))[:,np.newaxis])
 train_responses = np.vstack((text_responses, image_responses))
 
-svm = cv2.SVM()
-svm_params = dict(  kernel_type = cv2.SVM_LINEAR,
-                    # according to docs, linear kernel is the fastest; some tutorials recommend RBF
-                    svm_type = cv2.SVM_C_SVC,       # use C-SVM (standard)
-                    C=20.0,   # penalty term
-                    gamma=5.0 )
-
-# C, gamma are random guesses
-# probs should e.g. divide training data into to 2 sets and cross-validate
-svm.train(train_hist_data, train_responses, params=svm_params)
-
-# alternatively use train_auto, which should do cross-validation automatically (assuming I'm calling it correctly)
-# according to yaml dump, C is always 1.0?!
-svm_auto =cv2.SVM()
-svm_auto_params  = dict( kernel_type = cv2.SVM_LINEAR,svm_type = cv2.SVM_C_SVC)
-svm_auto.train_auto(train_hist_data, train_responses, None, None, params=svm_auto_params, k_fold=2)
-svm_auto.save('svm_auto.yaml')
-
-## Test prediction accuracy:
+## Retrieve test images for testing prediction accuracy:
 
 test_text_dir = "clips-217-texts-2/"
 test_image_dir = "clips-89-images-2/"
@@ -80,21 +62,71 @@ test_texthists = np.array(test_texthists)
 test_imagehists, _ = get_flathists(test_image_dir)
 test_imagehists = np.array(test_imagehists)
 
-# THE MAGIC
-text_test_preds = svm.predict_all(test_texthists)
-image_test_preds = svm.predict_all(test_imagehists)
+# loop over some guesses for C and gamma
 
-# remember TEXT_C == 0, IMAGE_C = 1, so we can simply sum things up
-nttext = len(text_test_preds)
-ntimag = len(image_test_preds)
-ncorrect_text = nttext - sum(text_test_preds)
-ncorrect_image = sum(image_test_preds)
-print "text recog accuracy", 1.0*ncorrect_text/nttext
-print "image recog accuracy", 1.0*ncorrect_image/ntimag
-print "total accuracy", (1.0*ncorrect_text +ncorrect_image)/(nttext + ntimag)
+C_range = np.logspace(0, 10, 15, base=2)
+gamma_range = np.logspace(-7, 4, 15, base=2)
+
+tcorrects = []
+icorrects = []
+
+for Cv in C_range:
+    for gammav in gamma_range:
+        svm = None
+        svm = cv2.SVM()
+        svm_params = dict(  kernel_type = cv2.SVM_LINEAR,
+                            # according to docs, linear kernel is the fastest; some tutorials recommend RBF
+                            svm_type = cv2.SVM_C_SVC,       # use C-SVM (standard)
+                            C=Cv,   # penalty term
+                            gamma=gammav )
+
+        # C, gamma are random guesses
+        # probs should e.g. divide training data into to 2 sets and cross-validate
+        svm.train(train_hist_data, train_responses, params=svm_params)
+
+
+        # THE MAGIC
+        text_test_preds = svm.predict_all(test_texthists)
+        image_test_preds = svm.predict_all(test_imagehists)
+
+        # remember TEXT_C == 0, IMAGE_C = 1, so we can simply sum things up
+        nttext = len(text_test_preds)
+        ntimag = len(image_test_preds)
+        ncorrect_text = nttext - sum(text_test_preds)
+        ncorrect_image = sum(image_test_preds)
+
+        tcorrects.append(ncorrect_text)
+        icorrects.append(ncorrect_image)
+        print Cv, gammav
+        print "text recog accuracy", 1.0*ncorrect_text/nttext
+        print "image recog accuracy", 1.0*ncorrect_image/ntimag
+        print "total accuracy", (1.0*ncorrect_text +ncorrect_image)/(nttext + ntimag)
 
 # objective: find such values for kernel type, C and nu  that we reach acceptable recognition accuracy
 # this could be automated, but we can get good enough results with just trying something
 
 # when done (= we have good svm), save it:
 svm.save("svm_final.yaml")    # we can load this in another script to do the large scale classifying
+
+
+# alternatively use train_auto, which should do cross-validation automatically (assuming I'm calling it correctly)
+# according to yaml dump, C is always 1.0?!
+svm_auto =cv2.SVM()
+svm_auto_params  = dict( kernel_type = cv2.SVM_LINEAR,svm_type = cv2.SVM_C_SVC)
+svm_auto.train_auto(train_hist_data, train_responses, None, None, params=svm_auto_params, k_fold=2)
+svm_auto.save('svm_auto.yaml')
+
+# THE MAGIC
+print "auto"
+text_test_preds = svm_auto.predict_all(test_texthists)
+image_test_preds = svm_auto.predict_all(test_imagehists)
+
+# remember TEXT_C == 0, IMAGE_C = 1, so we can simply sum things up
+nttext = len(text_test_preds)
+ntimag = len(image_test_preds)
+ncorrect_text = nttext - sum(text_test_preds)
+ncorrect_image = sum(image_test_preds)
+print Cv, gammav
+print "text recog accuracy", 1.0*ncorrect_text/nttext
+print "image recog accuracy", 1.0*ncorrect_image/ntimag
+print "total accuracy", (1.0*ncorrect_text +ncorrect_image)/(nttext + ntimag)
